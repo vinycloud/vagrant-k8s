624d89c59e32e033c4a81b95e89000e99b795e98
Vinicius Fialho <84024631+vinycloud@users.noreply.github.com>
2023-01-30 10:58:31 -0300

    Update node.sh

--- scripts/node.sh
+++ scripts/node.sh
@@ -4,7 +4,7 @@
 #                                                             # 
 # node.sh - Script for node configuration	                  #         
 #                                                             #
-# Actor: Vinicius Fialho - (vinytisolution@gmail.com)         #
+# Author: Vinicius Fialho - (vinytisolution@gmail.com)         #
 # Date created: 2023/01/25                                    # 
 #                                                             #
 # Description: This script configure node for k8s    	      #
